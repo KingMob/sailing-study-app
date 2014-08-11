11d63e6a3084bf029f887d744b5e71c395364b6d
Matthew Davidson <matthew@modulolotus.net>
2014-08-11 15:45:20 -0400

    Upgraded to Om 0.7.1, React 0.11.1, and cljs 2277

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/KingMob/sailing-study-app"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2268"]
+                 [org.clojure/clojurescript "0.0-2277"]
 ;                 [org.clojure/clojurescript "0.0-2234"] ; LightTable 0.6.3 has conn issues with higher versions
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [om "0.6.5"]]
+                 [om "0.7.1"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [lein-ring "0.8.11"]
